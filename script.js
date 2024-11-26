ff8fd7337a308d227e0d3de98d9a5726ae3b8193
Valux <valux.sh0p123@gmail.com>
2024-11-26 17:20:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Open the side menu
+function openMenu() {
+  document.getElementById("sideMenu").style.right = "0";
+}
+
+// Close the side menu
+function closeMenu() {
+  document.getElementById("sideMenu").style.right = "-250px";
+}
